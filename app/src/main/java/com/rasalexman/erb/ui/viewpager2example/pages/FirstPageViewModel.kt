36bfbb2f82a3d2be301e8fbcdd17ebaf4aee93ad
Alexander <sphc@yandex.ru>
2021-03-17 03:01:23 +0300

    preparing for publish to bintray

--- app/src/main/java/com/rasalexman/erb/ui/viewpager2example/pages/FirstPageViewModel.kt
+++ app/src/main/java/com/rasalexman/erb/ui/viewpager2example/pages/FirstPageViewModel.kt
@@ -1,18 +1,18 @@
 package com.rasalexman.erb.ui.viewpager2example.pages
 
+import androidx.lifecycle.MutableLiveData
 import com.rasalexman.easyrecyclerbinding.IBindingModel
 import com.rasalexman.erb.R
 import com.rasalexman.erb.ui.base.BaseViewModel
 
 class FirstPageViewModel : BaseViewModel(), IBindingModel {
 
+    val login: MutableLiveData<String> = MutableLiveData()
+    val password: MutableLiveData<String> = MutableLiveData()
+
 
     fun onSingInButtonClicked() {
-
-    }
-
-    fun onSingUpButtonClicked() {
-
+        println("------> onSingInButtonClicked login = ${login.value} password = ${password.value}")
     }
 
     override val layoutResId: Int
