16a4b2d8a7312c01c1a34d7625c9091df4f6278a
Alexander <sphc@yandex.ru>
2022-01-20 14:43:05 +0300

    remove build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,10 @@ rootProject.buildFileName = "build.gradle.kts"
 include(":app", ":easy-recycler-binding")
 rootProject.name = "EasyRecyclerBinding"
 
+/*
 buildCache {
     local {
         directory = File(rootDir, "build-cache")
         removeUnusedEntriesAfterDays = 2
     }
-}
+}*/
